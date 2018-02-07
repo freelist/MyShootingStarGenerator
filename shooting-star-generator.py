2874fd9bded07b21f40d4c29c0b37cae2089fa4d
Daniele Evangelista <danieleva2005@gmail.com>
2018-02-07 19:36:22 +0100

    updates

--- shooting-star-generator.py
+++ shooting-star-generator.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
+import pygame
 
+def playShooting():
+  pygame.mixer.init()
+  pygame.mixer.music.load("data/shooting-star.mp3")
+  pygame.mixer.music.play()
 def transparentOverlay(src, overlay, pos=(0,0)):
     h,w,_ = overlay.shape  
     rows,cols,_ = src.shape
@@ -14,19 +19,15 @@ def transparentOverlay(src, overlay, pos=(0,0)):
             src[x+i][y+j] = alpha*overlay[i][j][:3]+(1-alpha)*src[x+i][y+j]
     return src
 
-""" ----------- Read all images --------------------"""
+# play music
+playShooting()
+# load and work with images
 bImg = cv2.imread("data/background.png")
 pngImage = cv2.imread("data/foreground.png" , cv2.IMREAD_UNCHANGED)
-
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load("data/shooting-star.mp3")
-pygame.mixer.music.play()
 i = 0
 while pygame.mixer.music.get_busy() == True:
   print i
   result = transparentOverlay(np.copy(bImg), pngImage, (i, i))
-  #cv2.imwrite('./output/'+str(i)+'.jpg', result)
   #Display the result
   cv2.imshow("Result", result)
   cv2.waitKey(10)
